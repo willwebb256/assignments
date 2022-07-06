b618e9b348eb9bebfaeb86f65f332955e1ee8928
Will Webb <william.webb256@gmail.com>
2022-07-05 20:06:54 -0400

    redid the edit and remove actions to not use innerHTML

--- moms-shopping-list/index.js
+++ moms-shopping-list/index.js
@@ -5,53 +5,88 @@ removeToDo = document.querySelectorAll('.removeBtn'),
 form.addEventListener("submit", function(event){
     event.preventDefault()
 
-    const groceryItem = form.title.value
+    let groceryItem = form.title.value
     form.title.value = ""
     
     console.log(groceryItem)
 
     // 1. Defining what will be produced whenever you type an item and hit submit
 
-    const ul = document.createElement("ul")
+    const li = document.createElement("li")
 
     // 2. This will be the innerHTML stuff that you'll type into the thing
 
-    ul.innerHTML = "<ul class='list > li'>"+ groceryItem + "<br><button class='editBtn'>edit</button><button class='removeBtn'>X</button>" 
+    // li.innerHTML = "<p>"+ groceryItem + "<br><button class='editBtn'>edit</button><button class='removeBtn'>X</button></p>" 
+    var editBtn = document.createElement('button')
+        editBtn.classList.add('editBtn')
+    var removeBtn = document.createElement('button')
+        removeBtn.classList.add('removeBtn')
 
+    var paragraph = document.createElement('p')
+        paragraph.textContent= groceryItem
+        editBtn.textContent= 'Edit'
+        removeBtn.textContent='X'
+        li.append(paragraph, editBtn, removeBtn)
     //3. This is where we add the command to append after hitting the submit button! 
 
-    document.getElementById("list").append(ul)
+    document.getElementById("list").append(li)
+
+    
+        removeBtn.addEventListener("click", ()=>{
+            li.remove()
+        })
+
+    
 
 
-    var deletes = document.querySelectorAll('.removeBtn')
+    // var deletes = document.querySelectorAll('.removeBtn')
 
-    // Iterate all nodes
+    // // Iterate all nodes
 
-    deletes.forEach(btn => {
+    // deletes.forEach(btn => {
 
-    // Attach event listener. Note to preserve the button this-reference
-    // by using the non-shorthand function
+    // // Attach event listener. Note to preserve the button this-reference
+    // // by using the non-shorthand function
 
-     btn.addEventListener('click', function() {
-        var li = this.parentNode
-        li.remove()
-    })
+    //  btn.addEventListener('click', function() {
+    //     var li = this.parentNode
+    //     li.remove()
+    // })
 
 // edit button stuff:
         
-    var edits = document.querySelectorAll('.editBtn')
+   var edits = document.querySelectorAll('.editBtn')
+        edits.forEach(btn => {
+        btn.addEventListener('click', function() {
+        console.log("Edit Button Pushed")
+            var editForm = document.getElementById('editForm')
+            editForm.style.display= "block"
+            var editInput = document.getElementById('editInput')
+            editInput.value = paragraph.textContent
+            var saveButton = document.getElementById('saveButton')
+            saveButton.addEventListener('click', function(e) { 
+                
+                
+            e.preventDefault()
+            paragraph.textContent = editInput.value
+            editForm.style.display= "none"
+           // li.innerHTML = "<p>"+ groceryItem + "<br><button class='editBtn'>edit</button><button class='removeBtn'>X</button></p>"
+            console.log("Save Button Pushed")
+            console.log(groceryItem)
+        })
 
-    edits.forEach(btn => {
-        //event.preventDefault()
-        //btn.addEventListener("click", function() {
-        //event.preventDefault()
-        //var li = this.parentNode
-        //paragraph.contentEditable = true;
+            })
+        })
+        // event.preventDefault()
+        // btn.addEventListener("click", function() {
+        // event.preventDefault()
+        // var li = this.parentNode
+        // paragraph.contentEditable = true;
 
-    })
+    //})
         
   
-     console.log()   
+       
 
    
 
@@ -59,7 +94,6 @@ form.addEventListener("submit", function(event){
 
 
    
-})
 
 
 
