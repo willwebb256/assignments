22a5ed44f7f8a01b43df0f65f9128fd73cfd4c70
Will Webb <william.webb256@gmail.com>
2022-07-31 19:04:13 -0400

    did the problem solving practice

---
+++ problem-solving-practice/main.js
@@ -0,0 +1,18 @@
+// 1. Write a function that takes an array of numbers and returns the largest (without using Math.max())
+const largest = [3, 5, 2, 8, 1]; 
+const sortedLargest = largest.sort();
+// console.log(sortedLargest);
+console.log(sortedLargest[sortedLargest.length - 1])
+
+// 2. Write a function that takes an array of words and a character and returns each word that has that character present.
+const lettersWithStrings = ["#3", "$$$", "C%4!", "Hey!"];
+// No idea. lolz
+
+// 3. Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2.
+isDivisible(4, 2) // => true
+isDivisible(9, 3) // => true
+isDivisible(15, 4) // => false
+
+console.log(isDivisible(4, 2))
+
+// No Idea to that as well. 
