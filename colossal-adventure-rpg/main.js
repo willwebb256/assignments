880ce30c701a6345dbb0b7fbfb995b30dfcb4a58
Will Webb <william.webb256@gmail.com>
2022-08-08 18:15:29 -0400

    added the chalk and animation components to the rpg

--- colossal-adventure-rpg/main.js
+++ colossal-adventure-rpg/main.js
@@ -1,77 +1,188 @@
+#!/usr/bin/env node
 
-const readlineSync = require('readline-sync');
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+import keyIn from 'readline-sync';
+import readlineSync from 'readline-sync';
 
-// const rl = readlineSync.createInterface({
-//  input: process.stdin,
-//  output: process.stdout
-// })
+// Defined Objects and Functions:
 
-const name = readlineSync.question('Hey there young Pokemon Trainer! What is your name?\n');
+let playerName = {
+    userHealth: 40,
+    inventory: [],
 
-readlineSync.question('Top of the morning to ya ' + name + ', welcome to Pallet Town!\n This is where you will start your journey to become a Pokemon master.\n In order to battle and gain experience you will need to walk around in the vegetated areas to find Wild Pokemon!\n Then you can battle them to gain HP and experience.\n At any time you can also choose to exit or select print to see all of the data about your profile.\n Press Enter to begin. ');
-
-// readlineSync.promptCLLoop({
-//     p: function(target, into) {
-//       console.log('Name: ' + name + ': \n' + 'User Health: ' + userHealth + '\nTreasure: ' + 'Items: ' + pickUp);
-//       // Do something...
-//     },
-//     Print: function(target, into) {
-//       console.log('Name: ' + name + ': \n' + 'User Health: ' + userHealth + '\nTreasure: ' + 'Items: ' + pickUp);
-//       // Do something...
-//     },
-//     bye: function() { return true; }
-//   });
-//   console.log('Exited');
-
-// readlineSync.emitKeypressEvents(process.stdin)
-// process.stdin.on('keypress', (str, key) => {
-//     if (key.name === 'p') {
-//         process.stdout.write('Name: ' + name + ': \n' + 'User Health: ' + userHealth + '\nTreasure: ' + 'Items: ' + pickUp);
-// }})
-
-const wildPokemons = ['Rhydon', 'Alakazam', 'Electrode'];
-const treasure = ['1 entire Bitcoin', 'Redbull and a Chikfila Sandwich', 'A Tesla Model Y'];
-var prize = [];
-let userHealth = 30;
+}
+let enemies = [ 
+    {
+    enemyName: "Scyther",
+    wildPokemonHealth: 30,
+    treasure: "1 entire Bitcoin",
+},
+{
+    enemyName: "Alakazam",
+    wildPokemonHealth: 35,
+    treasure: "Redbull and a Chikfila Sandwich",
+},
+{
+    enemyName: "Porygon",
+    wildPokemonHealth: 25,
+    treasure: "Tesla Model Y",
+}
+]
+let prize = [];
 const options =['Walk', 'Exit', 'Print'];
-let pickUp = treasure[Math.floor(Math.random() * treasure.length)];
+// let pickUp = enemies.treasure[Math.floor(Math.random() * enemies.treasure)];
 
-function game() {
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+function rollEncounter(min, max) {
+    let encounterEnemy = Math.floor(Math.random()*3) === 1
+    if(!encounterEnemy){
+       let allClear = readline.keyIn("Nothing to see here, no wild Pokemon found."); //readline keyInPause --> nothing is found
+    }
+    if(encounterEnemy){
+        let enemy = enemies[math.floor]
+        let message = readline.keyIn( `youve encountered ${enemy.name}!`);
+        fightEnemy(enemy)
+    }
+    return Math.floor(Math.random() * (max-min) + min);
+}
+
+function wildPokemonEncounter(enemy) {
+    rollEncounter(1, 4)
+    if (diceRoll === 1) {
+        scyther = enemy
+    } else if (diceRoll === 2) {
+        alakazam = enemy
+    } else if (diceRoll === 3) {
+        porygon = enemy
+    }
+}
+
+function openInventory () {
+    for( let i = 0; playerName.inventory.length; i++ ){
+        console.log(playerName.inventory[i])
+    }
+}
+
+function calculateEnemyDamage () {
+    console.log(playerName.userHealth -= enemyAttack)
+}
+
+function checkUserHealth () {
+    console.log(playerName.userHealth)
+}
+
+function checkEnemyHealth () {
+    console.log(wildPokemonEncounter.wildPokemonHealth)
+}
+
+function attackWildPokemon () {
+    console.log(attackPower)
+}
+
+
+// These are the functions in order that will walk you through the game:
+
+// 1.
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    'Welcome to Pallet Town! Prepare to explore the Pokemon Multiverse! \n'
+  );
+  await sleep();
+  rainbowTitle.stop();
+    console.log(`
+    ${chalk.bgBlue('HOW TO PLAY')}
+    You are now in Pallet Town, a great place to start your journey to become the Ultimate Pokemon Master.
+    If you walk around, you might encounter a wild Pokemon and need to ${chalk.bgRed('BATTLE!')}
+    Sometimes you'll be able to run, but other times you won't be able to escape and will need to fight. 
+    \nIf you win, you'll get more HP and win some cool prizes. If you lose, you'll party in Valhalla with the Gods!
+    \nFeel free to type Print or p anytime to see your stats and inventory! Good Luck, and Catch 'em All!
+  `);
+}
+// 2.
+async function askName() {
+    const answers = await inquirer.prompt({
+        name: 'player_name',
+        type: 'input',
+        message: 'Before we get started, what is your name?',
+        default() {
+            return 'player';
+        },
+    });
+    playerName = answers.player_name;
+}
+// 3.
+async function question1() {
+    const answers = await inquirer.prompt ({
+       name: 'question_1', 
+       type: 'list',
+       message: 'What would you like to do?\n',
+       choices: [
+            'Walk',
+            'Exit',
+       ],
+    });
+    return handleAnswer(answers.question_1 == 'Walk');
+}
+    async function handleAnswer(isCorrect) {
+        const spinner = createSpinner('Sure thing, one second...').start();
+        await sleep();
+
+        if (isCorrect) {
+            spinner.success({ text: `Alright ${playerName}, let's roll!`});
+        } else {
+            spinner.error({ text: `😴😴😴 Boring ${playerName}, see you later!`});
+            process.exit(1);
+        }
+    }
+// 4. 
+
+    async function question2() {
+        const answers = await inquirer.prompt ({
+            name: 'question_2', 
+            type: 'list',
+            message: 'A Wild Pokemon Appeared! What would you like to do?\n',
+            choices: [
+                'Attack',
+                'Run',
+                'Print',
+            ],
+        });
+        
+        return handleAnswer2(answers.question_2 == 'Attack');
+    }
+     
+   
+     async function handleAnswer2(isCorrect) {
+                const spinner = createSpinner('Bold strategy, Cotton! Lets see how it pans out for ya!').start();
+                await sleep();
+        
+                if (isCorrect) {
+                    spinner.success({ text: `Nice work ${playerName}, your Pokemon used Quick Attack! caused 30 Damage Points to enemy Pokemon!`});
+                } else {
+                    spinner.error({ text: `Wow ${playerName}, you got away safely!`});
+                    process.exit(1);
+                }
+            }
+// 5. 
+    function game() {
     const attackPower = Math.floor(Math.random() * (8 - 1) + 2);
-    const wildPokemon = wildPokemons[Math.floor(Math.random() * wildPokemons.length)];
-    let wildPokemonHealth = 30;
     const wildPokemonPower = Math.floor(Math.random() * (8 - 3) + 2);
-
-    const index = readlineSync.keyInSelect(options, 'What is next on the horizon for you?');
-
-    if (options[index] == 'Exit') {
-        return userHealth = 0;
-    } else if (options[index] == 'Print') {
-        console.log('Name: ' + name + ': \n' + 'User Health: ' + userHealth + '\nTreasure: ' + 'Items: ' + pickUp);
-    } else if (options[index] === 'Walk') {
         let key = Math.random();
-        if (key <= .3) {
+            if (key <= .3) {
             console.log('Walking around and no sign of wild Pokemon here so far.');
         } else if (key >= .3) {
-            console.log('A wild ' + wildPokemon + ' has appeared!\n What would you like to do?');
+            console.log('A wild ' + wildPokemonEncounter + ' has appeared!\n What would you like to do?');
 
-            while(wildPokemonHealth > 0 && userHealth > 0) {
-
-                // readlineSync.emitKeypressEvents(process.stdin);
-                //     if (process.stdin.isTTY)
-                //     process.stdin.setRawMode(true);
-
-                //     process.stdin.on('keypress', (str, key) => {
-                //         if(key.name == 'Print' || key.name == 'p'){
-                //             console.log('Name: ' + name + ': \n' + 'User Health: ' + userHealth + '\nTreasure: ' + 'Items: ' + pickUp);
-                //         }
-                //         // console.log(str)
-                //         // console.log(key)
-                //         })
+            while(wildPokemonHealth > 0 && playerName.userHealth > 0) {
 
                 const user = readlineSync.question('Enter "r" to run, or enter "a" to attack! ');
-
-                switch (user) {
+                    switch (user) {
                     case 'r':
                         const run = Math.random();
                         if (run < .5) {
@@ -89,58 +200,59 @@ function game() {
                                            
                         if (wildPokemonHealth <= 0) {
                             console.log('Nice work! You defeated ' + wildPokemon + '.\n' + wildPokemon + ' left behind: ' + pickUp);
-                            let loot = Math.random();
+                            let loot = enemies.treasure();
                             if (loot <= .3) {
                                 prize.push(pickUp);
                             }
                         } else if (userHealth <= 0) {
                             console.log(wildPokemon + ' has defeated your Pokemon. You blacked out! You will respawn at the nearest Pokemon Center.');
-                        }
+                           }
                 }
             }
         }
     }
+    
+    //             let inGame = true;
+    //             let inBattle = false;
+
+                   // const index = readlineSync.keyInSelect(options, 'What is next on the horizon for you?');
+
+    // if (options[index] == 'Exit') {
+    //     playerName.userHealth = 0;
+    // } else if (options[index] == 'Print') {
+    //     console.log(playerName);
+    // } else if (options[index] === 'Walk') {
+
+    //            
+
+// 6.
+function winner() {
+    console.clear();
+    const msg = `congrats, ${playerName}!\n You are a Poke MASTER!`;
+
+    figlet(msg, (err, data) => {
+        console.log(gradient.pastel.multiline(data));
+    });
 }
 
-while(userHealth > 0) {
-    userRestore = function() {
-        userActive = true;
-        userHealth = 30;
-    };
-    userRestore();
-    game();
-}
 
-// Some Issues I am having: 
-// After you run away successfully, it doesnt return you to the main menu. 
-// You are stuck in the run or attack decision loop. 
-// Also - I cant figure out a way to make it print on command at any time. 
 
-// Proposed solution that didnt work:
-// case 'p':
-//     console.log('Name: ' + name + ': \n' + 'User Health: ' + userHealth + '\nTreasure: ' + 'Items: ' + pickUp);
+// Other things that help make this game run:
 
-// The year is 1985. Your job is to build a text-based (console) RPG game.
+// while(playerName.userHealth > 0) {
+//     userRestore = function() {
+//         userActive = true;
+//         userHealth = 40;
+//     }};
+//     userRestore();
+// game();
 
-// The point of this exercise is to better master Javascript functions, loops, conditionals, and some data types.
 
-// ### **Project Requirements:**
+await welcome();
+await askName();
+await question1();
+await question2();
+await game();
+await winner();
 
-// 1. Console must greet player with a fun message
-// 2. Console must ask for the player's name and store it
-// 3. Walking:
-// - The console will ask the user to enter a "w" to walk
-// - Every time the player walks, a random algorithm will be run that determines if a wild enemy has appeared (A 1/3 or 1/4 chance of being attacked)
-// - Use a while loop to control this flow.
-// 4. If a wild enemy appears:
-// - The enemy is random (can be chosen out of a minimum of 3 different enemy names)
-// - The user can decide to attack or run
-// - If attacking, a random amount of damage will be dealt between a min and max
-// - If running, there will be a 50% chance of escaping
-// - After the player attacks or runs the enemy attacks back for a random damage amount
-// - The player and enemy will attack each other in a loop until one of them passes out or dies.
-// - If the player kills the enemy you can give the Player some HP and a special item that is stored in the inventory. After this, the player will continue walking.
-// - If the enemy kills the player the console prints a cool death message and the game ends
-// 5. Inventory
-// - When the player kills enemies, they are awarded with items
-// - If the user enters 'Print' or 'p' in the console, the console will print the players name, HP, and each item in their inventory
+
