9cbb4653d97b9fce16bec1d0a844e48c42719b7e
Will Webb <william.webb256@gmail.com>
2022-08-30 10:25:21 -0400

    completed the catch me if you can exercise

--- catch-me-if-you-can/main.js
+++ catch-me-if-you-can/main.js
@@ -20,23 +20,41 @@
 // Use console.log within a catch block to inform the user of the error.
 
 
-let x = 10
-let y = 5
+// let x = 10
+// let y = 5
+
+// try{
+// function sum(x,y){
+//     if(isNaN(x)){
+//         throw new Error("X is Not a Number!");
+//     } else if(isNaN(y)){
+//         throw new Error("Y is Not a Number!");
+//     } return x + y
+//     }
+//     }
+
+//     catch(err){
+//         console.log(err)
+//     }
+
+// console.log(sum(x, y))
+
+// 2a: Given a user object, write a function called login that takes a username and password
+// as parameters. Throw an error if either of them don't match. Otherwise, log to the 
+// console a message saying "login successful!"
+
+var user = {username: "sam", password: "123abc"};
 
 try{
-function sum(x,y){
-    if(isNaN(x)){
-        throw new Error("X is Not a Number!");
-    } else if(isNaN(y)){
-        throw new Error("Y is Not a Number!");
-    } return x + y
-    }
-    }
+function login(username, password){
+  //check credentials
+  if( user.username === "sam" && user.password === "123abc"){
+        return "Login Successful!"
+  } else throw new Error ("Incorrect Login")
+}}
+catch(err){
+    console.log(err)
+}
 
-    catch(err){
-        console.log(err)
-    }
+console.log(login(user))
 
-console.log(sum(x, y))
-
-// 2a: 
