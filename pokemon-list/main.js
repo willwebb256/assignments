5f1d5f7912aad4314098470cf60961f95e2d4491
Will Webb <william.webb256@gmail.com>
2022-09-22 17:59:46 -0400

    started pokemon list

---
+++ pokemon-list/main.js
@@ -0,0 +1,34 @@
+// xhr.onreadystatechange
+// xhr.readyState
+// xhr.status
+// xhr.responseText
+ 
+// xhr.open()
+// xhr.send()
+ 
+// url: https://api.vschool.io/pokemon
+ 
+ 
+const xhr = new XMLHttpRequest()
+       // method  // url                     // async?
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send()
+ 
+xhr.onreadystatechange = function(){
+   if(xhr.readyState === 4 && xhr.status === 200){
+       const JSONdata = xhr.responseText
+       const data = JSON.parse(JSONdata)
+       showData(data.results)
+   
+   }
+}
+ 
+ 
+function showData(data){
+   for(let i = 0; i < data.length; i++){
+       const h1 = document.createElement('h1')
+       h1.textContent = (data.objects[0]);
+       document.body.appendChild(h1)
+   }
+}
+
