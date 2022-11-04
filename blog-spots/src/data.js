20f69b240186cd775f5e3e6a053b0d5360490797
Will Webb <william.webb256@gmail.com>
2022-11-04 09:21:48 -0400

    completed solo project two and made new folder for blog spots

---
+++ blog-spots/src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        title: "Kühtai",
+        location: "Austria",
+        googleMapsUrl: "https://goo.gl/maps/agEBmL8hXR63Y2xk6",
+        startDate: "12 Jan, 1996",
+        endDate: "24 Jan, 1996",
+        description: "A white snow splendor inspires in Kühtai from the beginning of the season until well into spring. Perfectly groomed pistes make athletes' hearts beat faster. And a glistening white mountain panorama ensures feelings of exhilaration. Let the adventure begin. With winter experiences at over 2,020 meters just waiting to put a smile on your face. Because one thing is certain: in Kühtai there is snow as far as the eye can see.",
+        imageUrl: "./assets/images/austria.png"
+    },
+            {
+        id: 2,
+        title: "Haleiwa",
+        location: "Hawaii",
+        googleMapsUrl: "https://goo.gl/maps/wD8bzSKGRVP9LpDa7",
+        startDate: "20 Dec 2017",
+        endDate: "30 Dec, 2017",
+        description: "Rich with island history, Haleʻiwa is now the social and artistic hub of the North Shore. Here you’ll find surfers fueling up on shrimp or other delicacies at one of the town’s abundant food trucks before hitting the famous beaches of Waimea Bay, ʻEhukai (Banzai Pipeline) and Sunset Beach. You’ll also find locals and visitors winding down with a shaved ice after a day in the sun or shopping at boutiques filled with unique gifts that will allow you to bring back a piece of Hawaiʻi with you. Haleʻiwa is a far cry from the excitement of Waikīkī, and that’s exactly how the people of the North Shore like it.",
+        imageUrl: "./assets/images/hawaii.png"
+    },
+            {
+        id: 3,
+        title: "Isla Mujeres",
+        location: "Mexico",
+        googleMapsUrl: "https://goo.gl/maps/J9yGgwobQo6KgZFA9",
+        startDate: "15 Dec, 2019",
+        endDate: "23 Dec, 2019",
+        description: "Known for beautiful white sand beaches, calm crystal clear turquoise waters and the friendliest locals in the Mexican Caribbean. Isla’s diversity, from the fisherman, artists and musicians to divers, chefs and ex-pats, make up a community unlike any other. With the beauty of the Caribbean, the heritage of the Mayans, and the easygoing charm of the people, Isla Mujeres has become a haven for people from all over the world.",
+        imageUrl: "./assets/images/mexico.png"
+    }
+]
---
+++ solo-project-two/src/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Globe from "./assets/images/globe.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={Globe} className="nav--logo" />   
+            <span className="nav--text">my travel journal.</span>
+        </nav>
+    )
+}
